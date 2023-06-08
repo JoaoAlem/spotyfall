38a3b384fe498058a77498848b89435d4a95e645
joao <joao.aalem@gmail.com>
2023-06-08 20:40:07 -0300

    feat: error and success components

---
+++ src/main/webapp/WEB-INF/public/components/error.js
@@ -0,0 +1,8 @@
+export default {
+    template:`
+        <div id="balaoErro" class="balao">
+            <i class="fas fa-times-circle"></i>
+            Erro ao realizar teste!
+        </div>
+    `
+}
---
+++ src/main/webapp/WEB-INF/public/components/success.js
@@ -0,0 +1,8 @@
+export default {
+    template:`
+        <div id="balaoSucesso" class="balao">
+            <i class="fas fa-check-circle"></i>
+            Alterado com sucesso!
+        </div>
+    `
+}
