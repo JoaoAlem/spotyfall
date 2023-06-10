0e2b3e2b83e7e8d30ccc50090bd43fde8e9c0a76
joao <joao.aalem@gmail.com>
2023-06-10 14:22:00 -0300

    feat: added liked and following tables

--- src/main/java/com/player/spotyfall/models/UserModel.java
+++ src/main/java/com/player/spotyfall/models/UserModel.java
@@ -4,10 +4,20 @@ import com.player.spotyfall.modules.database.Database;
 
 public class UserModel {
     /* Dados do banco de dados */
-    private final String[] colunas = new String[]{"id_user", "name", "surname", "username", "email", "phone", "userImage"};
+    private final String[] colunas_user = new String[]{"id_user", "name", "surname", "username", "email", "phone", "userImage"};
+    private final String[] colunas_liked = new String[]{"id_music", "id_user"};
+    private final String[] colunas_following = new String[]{"id_artist", "id_user"};
 
     /* Metodos do banco de dados */
     public Database users(){
-        return new Database("users").Columns(colunas);
+        return new Database("users").Columns(colunas_user);
+    }
+
+    public Database liked(){
+        return new Database("liked").Columns(colunas_liked);
+    }
+
+    public Database following(){
+        return new Database("artistsFollowed").Columns(colunas_following);
     }
 }
