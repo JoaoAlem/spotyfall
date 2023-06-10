4cae69359f223b7509147cfee11b9048f7ad12bd
joao <joao.aalem@gmail.com>
2023-06-10 14:22:19 -0300

    refact: change table from model

--- src/main/java/com/player/spotyfall/models/MusicModel.java
+++ src/main/java/com/player/spotyfall/models/MusicModel.java
@@ -5,14 +5,11 @@ import com.player.spotyfall.modules.database.Database;
 public class MusicModel {
     /* Dados do banco de dados */
     private final String[] colunas_musics = new String[]{"id_music", "musicName", "musicImage"};
-    private final String[] colunas_musicAuthor = new String[]{"id_music", "id_artist"};
 
     /* Metodos do banco de dados */
     public Database musics(){
         return new Database("musics").Columns(colunas_musics);
     }
 
-    public Database musicAuthor(){
-        return new Database("musics").Columns(colunas_musicAuthor);
-    }
+
 }
