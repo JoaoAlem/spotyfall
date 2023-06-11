6d5fed6051c0153d6fde8ffee25a3b52d45585c7
joao <joao.aalem@gmail.com>
2023-06-11 20:40:21 -0300

    feat: addition of a modal for editing

---
+++ src/main/webapp/WEB-INF/public/components/modalEdit.js
@@ -0,0 +1,35 @@
+export default {
+    template: `
+    <div style="margin: 0 !important;" id="modal-edit" class="fixed  h-screen w-screen top-2/4 left-2/4 -translate-y-1/2 -translate-x-1/2 hidden z-10">
+        <!-- background -->
+        <div class="opacity-25 bg-black h-screen w-screen absolute top-0 left-0 z-0"></div>
+        
+        <!-- form -->
+        <form @submit.prevent style="background-color: var(--sobretomRoxo)" class="flex flex-col z-10 m-auto text-white">
+            <!-- Form Title -->
+            <div class="flex">
+                <h2 class="mr-auto ml-2" v-if="false">Editando {{item.title}}</h2>
+                <h2 class="mr-auto ml-2" v-else>Adicionar item</h2>
+                <i class="fa fa-close w-6 ml-auto mr-2"></i>
+            </div>
+            
+            <!-- Form body -->
+            <div class="flex flex-col">
+                <label for="form-title">Titulo da música</label>
+                <input id="form-title" type="text">
+            </div>
+            
+            <!-- Form footer -->
+            <div class="flex flex-row">
+                <button @click="this.$root.closeModal('modal-edit')" class="w-10 h-7 bg-gray-500"><i class="fa fa-close"></i></button>
+                <button @click="save" class="w-10 h-7 bg-green-400"><i class="fa fa-check"></i></button>
+            </div>
+        </form>
+    </div>
+    `,
+    methods: {
+        save(){
+
+        }
+    }
+}
