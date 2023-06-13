040e3ba6415ea8f88b332c94d165fafc4abf11e9
joao <joao.aalem@gmail.com>
2023-06-12 21:57:57 -0300

    fix: change in put and fixes around file

--- src/main/java/com/player/spotyfall/modules/database/Database.java
+++ src/main/java/com/player/spotyfall/modules/database/Database.java
@@ -51,7 +51,8 @@ public class Database {
     ===============================================
      */
 
-    /** Função que vai executar a query do banco de dados, ainda somente selects
+    /**
+     * Função que vai executar a query do banco de dados, ainda somente selects
      *
      * @return ResultSet com oque foi encontrado do banco de dados
      * @throws SQLException um erro do SQL
@@ -61,36 +62,47 @@ public class Database {
         return ps.executeQuery();
     }
 
-    /** Função que vai executar a query do banco de dados, ainda somente selects
+    /**
+     * Função que vai executar a query do banco de dados, ainda somente selects
      *
      * @throws SQLException um erro do SQL
      */
     private void executeUpdate() throws SQLException {
         PreparedStatement ps = this.conn.prepareStatement(this._query);
+
+        if (this._values != null) {
+            String[] valueArray = this._values.split(", ");
+            for (int i = 0; i < valueArray.length; i++) {
+                ps.setString(i + 1, valueArray[i]);
+            }
+        }
+
         ps.executeUpdate();
     }
 
-    /** Define as colunas desejadas para o objeto do banco de dados
+    /**
+     * Define as colunas desejadas para o objeto do banco de dados
      *
      * @param colunas nome das colunas em um Array
      * @return um objeto do banco de dados
      */
-    public Database Columns(String[] colunas){
+    public Database Columns(String[] colunas) {
         this._columns = colunas;
         return this;
     }
 
-    /** Método para limpar a query
-     *
+    /**
+     * Método para limpar a query
      */
-    private void Clear(){
+    private void Clear() {
         this._whereConditions = new ArrayList<>();
         this._query = "";
         this._filters = new ArrayList<>();
     }
 
 
-    /** método para fechar a conexão
+    /**
+     * método para fechar a conexão
      *
      * @throws SQLException
      */
@@ -98,8 +110,8 @@ public class Database {
         this.conn.close();
     }
 
-    /** Método que limpa e fecha a conexão
-     *
+    /**
+     * Método que limpa e fecha a conexão
      */
     public void Sanitize() throws SQLException {
         Clear();
@@ -112,17 +124,18 @@ public class Database {
     ===============================================
      */
 
-    /** Função que irá "construir" as condições where, porem somente com o operador AND
+    /**
+     * Função que irá "construir" as condições where, porem somente com o operador AND
      *
      * @return Instancia do objeto Database
      */
-    private StringBuilder getWhere(){
+    private StringBuilder getWhere() {
         String operator = "";
         StringBuilder whereConditionsBuilder = new StringBuilder();
         for (String[] condition : this._whereConditions) {
-            if(condition.length != 1) {
+            if (condition.length != 1) {
                 // definindo o operador e atualizando o condition atual
-                operator = condition[condition.length -1];
+                operator = condition[condition.length - 1];
                 condition = Arrays.copyOf(condition, condition.length - 1);
             }
 
@@ -140,42 +153,47 @@ public class Database {
         return whereConditionsBuilder;
     }
 
-    /** Método usado pela instancia do banco de dados para
-     *  definir as condições where e retornar a instancia
+    /**
+     * Método usado pela instancia do banco de dados para
+     * definir as condições where e retornar a instancia
+     *
      * @param item array de condições
      * @return instancia do objeto Database
      */
-    private Database setWhere(String[] item){
+    private Database setWhere(String[] item) {
         this._whereConditions.add(item);
         return this;
     }
 
 
-    /** Define uma condição "Where column is null"
+    /**
+     * Define uma condição "Where column is null"
      *
      * @param column a coluna alvo
      * @return um objeto do banco de dados
      */
-    public Database WhereNull(String column){
+    public Database WhereNull(String column) {
         String[] item = new String[1];
         item[0] = column + " IS NULL";
 
         return setWhere(item);
     }
 
-    /** Define uma condição "Where column is not null"
+    /**
+     * Define uma condição "Where column is not null"
      *
      * @param column a coluna alvo
      * @return um objeto do banco de dados
      */
-    public Database WhereNotNull(String column){
+    public Database WhereNotNull(String column) {
         String[] item = new String[1];
         item[0] = column + " IS NOT NULL";
 
         return setWhere(item);
     }
 
-    /** Define uma condição where
+    /**
+     * Define uma condição where
      *
      * @param condition a condição desejava
      * @return uma instancia do objeto database
@@ -183,49 +201,50 @@ public class Database {
      */
     public Database Where(String condition) throws databaseFault {
         String[] item = new String[2];
-        if(Utils.validateString(condition)) {
+        if (Utils.validateString(condition)) {
             item[0] = condition;
             item[1] = "AND";
-        }
-        else
+        } else
             throw new databaseFault("Fill the fields");
 
         return setWhere(item);
     }
 
-    /** Define uma condição "where column = value"
+    /**
+     * Define uma condição "where column = value"
      *
      * @param column coluna desejada
-     * @param value valor da coluna
+     * @param value  valor da coluna
      * @return instancia do objeto database
      * @throws databaseFault Pode retornar um erro de string vazia
      */
     public Database Where(String column, String value) throws databaseFault {
         String[] item = new String[4];
-        if(Utils.validateString(column) && Utils.validateString(value)){
+        if (Utils.validateString(column) && Utils.validateString(value)) {
             item[0] = column;
             item[1] = "=";
             item[2] = '"' + value + '"';
             item[3] = "AND";
-        }else{
+        } else {
             throw new databaseFault("Fill the fields");
         }
 
         return setWhere(item);
     }
 
-    /** Define uma condição "where column operator value"
+    /**
+     * Define uma condição "where column operator value"
      *
-     * @param column coluna desejada
+     * @param column   coluna desejada
      * @param operator operador da operação
-     * @param value valor desejado
+     * @param value    valor desejado
      * @return instancia do objeto databse
      * @throws databaseFault Pode retornar um erro de string vazia
      */
     public Database Where(String column, String operator, String value) throws databaseFault {
         String[] item = new String[4];
 
-        if(Utils.validateString(column) && Utils.validateString(operator) && Utils.validateString(value)){
+        if (Utils.validateString(column) && Utils.validateString(operator) && Utils.validateString(value)) {
             item[0] = column;
             item[1] = operator;
             item[2] = '"' + value + '"';
@@ -236,7 +255,8 @@ public class Database {
     }
 
 
-    /** Executa um where com a condição or
+    /**
+     * Executa um where com a condição or
      *
      * @param column
      * @param value
@@ -245,12 +265,12 @@ public class Database {
      */
     public Database WhereOr(String column, String value) throws databaseFault {
         String[] item = new String[4];
-        if(Utils.validateString(column) && Utils.validateString(value)){
+        if (Utils.validateString(column) && Utils.validateString(value)) {
             item[0] = column;
             item[1] = "=";
             item[2] = '"' + value + '"';
             item[3] = "OR";
-        }else{
+        } else {
             throw new databaseFault("Fill the fields");
         }
 
@@ -262,12 +282,13 @@ public class Database {
     ===============================================
      */
 
-    /** Método para realizar um select com todos os itens da tabela
+    /**
+     * Método para realizar um select com todos os itens da tabela
      *
      * @return ResultSet com os resultados obtidos
      * @throws SQLException erro do sql
      */
-    private Map<String, String> SelectMap(){
+    private Map<String, String> SelectMap() {
         Map<String, String> _query = new HashMap<>();
         _query.put("columns", String.join(", ", this._columns));
         _query.put("where", this.getWhere().toString());
@@ -277,7 +298,8 @@ public class Database {
         return _query;
     }
 
-    /** Função que executa um Select e retorna um ResultSet
+    /**
+     * Função que executa um Select e retorna um ResultSet
      *
      * @return ResultSet com os dados consultados
      * @throws SQLException
@@ -289,7 +311,8 @@ public class Database {
         return _Select();
     }
 
-    /** Função que executa um Select e retorna um ResultSet com somente 1 resultado
+    /**
+     * Função que executa um Select e retorna um ResultSet com somente 1 resultado
      *
      * @return ResultSet com os dados consultados
      * @throws SQLException
@@ -301,7 +324,8 @@ public class Database {
         return _Select();
     }
 
-    /** Método que executa um select e retorna em json
+    /**
+     * Método que executa um select e retorna em json
      *
      * @return
      * @throws SQLException
@@ -338,56 +362,73 @@ public class Database {
                   INSERT E UPDATE
     ===============================================
      */
+    private String _saveColumns = null;
+    private String _values = null;
+
+    public int _validateFields(Map<String, Object> data) {
+        int columnCount = 0;
+        for (Map.Entry<String, Object> entry : data.entrySet()) {
+            String columnName = entry.getKey();
+            Object columnValue = entry.getValue();
+
+            if (!columnValue.equals("null") && Utils.validateString(columnValue.toString())) {
+                if (columnCount > 0) {
+                    this._saveColumns += ", ";
+                    this._values += ", ";
+                }
+                this._saveColumns += columnName;
+                this._values += columnValue;
+                columnCount++;
+            }
+        }
+        return columnCount;
+    }
 
     public void Put(Map<String, Object> data, String id) throws SQLException {
         Map<String, String> _queryHelper = new HashMap<>();
         _queryHelper.put("table", this.table);
 
-        if(Utils.validateString(id)){
-            int columnCount = 0;
+        if (Utils.validateString(id)) {
+            int columnCount = _validateFields(data);
 
             _queryHelper.put("where", this.getWhere().toString());
+            _queryHelper.put("saveColumns", this._saveColumns);
+            _queryHelper.put("values", this._values);
 
             this._query = "UPDATE ${table} SET ";
-            for (Map.Entry<String, Object> entry : data.entrySet()) {
-                String columnName = entry.getKey();
-                Object columnValue = entry.getValue();
 
-                this._query += columnName + " = " + columnValue + ", ";
-                columnCount++;
-            }
-
-            // remover a virgula extra
+            // Remove the extra comma
             if (columnCount > 0) {
-                this._query = this._query.substring(0, this._query.length() - 2);
+                this._query += "${saveColumns};";
+            } else {
+                this._query = "";
             }
-
-            this._query += " ${where};";
-
-            StringSubstitutor substitutor = new StringSubstitutor(_queryHelper);
-            this._query = substitutor.replace(this._query);
-        } else{
+        } else {
             this._query = "INSERT INTO ${table} (";
-            for (Map.Entry<String, Object> entry : data.entrySet()) {
-                String columnName = entry.getKey();
-                this._query += columnName + ", ";
+
+            // Initialize _saveColumns and _values
+            this._saveColumns = "";
+            this._values = "";
+
+            int columnCount = _validateFields(data);
+
+            // Append _saveColumns to the query
+            this._query += this._saveColumns;
+
+            if (columnCount > 0) {
+                this._query += ") VALUES (";
+
+                // Append _values with placeholders
+                String[] placeholders = new String[columnCount];
+                Arrays.fill(placeholders, "?");
+                this._query += String.join(",", placeholders);
             }
 
-            // remover a virgula extra
-            this._query = this._query.substring(0, this._query.length() - 2);
-            this._query += ") VALUES (";
-
-            for (Map.Entry<String, Object> entry : data.entrySet()) {
-                Object columnValue = entry.getValue();
-                this._query += "'" +  columnValue + "', ";
-            }
-
-            this._query = this._query.substring(0, this._query.length() - 2);
             this._query += ");";
-
-            StringSubstitutor substitutor = new StringSubstitutor(_queryHelper);
-            this._query = substitutor.replace(this._query);
         }
+
+        StringSubstitutor substitutor = new StringSubstitutor(_queryHelper);
+        this._query = substitutor.replace(this._query);
 
         this.executeUpdate();
     }
