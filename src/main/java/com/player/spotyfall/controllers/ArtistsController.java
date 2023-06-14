6e006d55f9fb89a0aef0b50a13bc98e44883da91
joao <joao.aalem@gmail.com>
2023-06-13 21:31:22 -0300

    feat: added artistsController

---
+++ src/main/java/com/player/spotyfall/controllers/ArtistsController.java
@@ -0,0 +1,21 @@
+package com.player.spotyfall.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ArtistsController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+}
